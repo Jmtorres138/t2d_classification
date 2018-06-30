bb70ebb1b27387fe3c2062e4fb62c4e6c52e2852
Jason Torres <jason.matthew.torres@gmail.com>
2018-06-30 19:24:12 +0100

    weighted version

--- method_C/eQTL-enrichment_specific-downsampled.R
+++ method_C/eQTL-enrichment_specific-downsampled.R
@@ -189,13 +189,13 @@ cred.dir <- serv.dir2 %&% "projects/t2d_classification/method_C/genetic_credible
 cred.df <- fread(cred.dir %&% "gencred.txt") %>% filter(.,PPA>=0.01)
 # PPA 1% filter reduces number of query SNPs from 126K to 5,010 SNPs 
 
-input1.df <- fread(proj.dir %&% "method_C/analysis_files/classified-loci_unweighted.txt")
-thresh1.df <- evaluate_thresholds(input1.df,iter=1000)
-write.table(x=thresh1.df,file=proj.dir%&%"method_C/analysis_files/eqtl-specific-validation_unweighted.txt",
-            sep="\t",quote=FALSE,row.names=FALSE)
-
-#input2.df <- fread(proj.dir %&% "method_C/analysis_files/classified-loci_weighted.txt")
-#thresh2.df <- evaluate_thresholds(input2.df,iter=1000)
-#write.table(x=thresh2.df,file=proj.dir%&%"method_C/analysis_files/eqtl-specific-validation_weighted.txt",
+#input1.df <- fread(proj.dir %&% "method_C/analysis_files/classified-loci_unweighted.txt")
+#thresh1.df <- evaluate_thresholds(input1.df,iter=1000)
+#write.table(x=thresh1.df,file=proj.dir%&%"method_C/analysis_files/eqtl-specific-validation_unweighted.txt",
 #            sep="\t",quote=FALSE,row.names=FALSE)
+
+input2.df <- fread(proj.dir %&% "method_C/analysis_files/classified-loci_weighted.txt")
+thresh2.df <- evaluate_thresholds(input2.df,iter=1000)
+write.table(x=thresh2.df,file=proj.dir%&%"method_C/analysis_files/eqtl-specific-validation_weighted.txt",
+            sep="\t",quote=FALSE,row.names=FALSE)
 
