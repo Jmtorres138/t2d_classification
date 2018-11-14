53b5449737a5add30058636384f51923ea23ff56
Jason Torres <jmtorres138@gmail.com>
2018-11-14 18:19:21 +0000

    added

---
+++ revamp/00.02b_specific.py
@@ -0,0 +1,19 @@
+import re
+fin = open("specific-bed-files.txt",'r')
+fout = open("specific_processed.txt",'w')
+count = 0
+
+for entry in fin:
+	f = entry.strip()
+	print f
+	fi = open(f,'r')
+	for line in fi:
+		l = line.strip().split()
+		if len(l) == 4:
+			fout.write("\t".join(l)+"\n")
+		else:
+			count += 1
+	fi.close()
+print count 
+fin.close()
+fout.close()
