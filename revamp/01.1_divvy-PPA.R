1bb75a605efd9dabe94ff0abd820765dc7f8c1e7
Jason Torres <jmtorres138@gmail.com>
2018-11-14 16:45:01 +0000

    copied files over from method_C

---
+++ revamp/01.1_divvy-PPA.R
@@ -0,0 +1,240 @@
+# Setup
+
+
+
+"%&%" <- function(a,b) paste0(a,b)
+
+library("tidyverse")
+library("data.table")
+library("Homo.sapiens")
+
+serv.dir <- "/well/mccarthy/users/jason/"
+proj.dir <- serv.dir %&% "projects/t2d_classification/"
+work.dir <- proj.dir %&% "method_C/"
+out.dir <- proj.dir %&% "method_C/analysis_files/"
+res.dir <- work.dir %&% "genetic_credible_sets/" #"null_results/"
+input.dir <- proj.dir %&% "analysis_files/"
+
+sym.ids <- unique(keys(Homo.sapiens, keytype = "SYMBOL"))
+sym.df <- select(Homo.sapiens,key=sym.ids,keytype="SYMBOL",
+                 columns=c("ENSEMBL"))
+
+ess.dir <- proj.dir %&% "analysis_files/"
+ess.df <- fread(ess.dir %&% "expression_specificity_scores.txt")
+#ess.df <- fread(ess.dir %&% "expression_specificity_scores-rntransform.txt")
+
+weight.all.df <- fread(proj.dir %&%  "method_A/analysis_files/weight-enrich-all.txt")
+
+
+
+
+
+#blk.df <- fread(res.dir %&% "null_results_blocks.txt")
+cred.df <- fread(res.dir %&% "gencred.txt")
+bed1.df <- fread(input.dir %&% "all_shared.bed")
+bed2.df <- fread(input.dir %&% "shared_processed.txt")
+shared.df <- rbind(bed1.df,bed2.df); rm(bed1.df); rm(bed2.df)
+specific.df <- fread(input.dir %&% "specific_processed.txt")
+states.df <- rbind(shared.df,specific.df)
+
+
+# Append necessary information 
+
+#mult.df <- fread(proj.dir %&%  "method_A/multi_results/results_func-cred-sets.txt")
+
+#fgwas.path <- "/well/got2d/jason/projects/t2d-integration/fgwas/" %&% 
+#  "diagram_hrc/cross_tissue/multi_tissue_joint_analysis/fgwas_input/" %&% 
+#  "ukbb_diamante-euro.fgwas.gz"
+
+#fgwas.df <- fread("cat " %&% fgwas.path %&% " | zmore")
+#names(fgwas.df)[names(fgwas.df)=="distance_tss"] <- "distance_tss_0_5000"
+#fgwas.df <- dplyr::select(fgwas.df,one_of(names(mult.df))) %>% 
+#  dplyr::select(.,-one_of("CHR","POS","Z"))
+
+#cred.df <- inner_join(cred.df,fgwas.df,by="SNPID")
+
+# Divvy function
+
+locid_to_symbol <- function(loc.id){
+  return(filter(cred.df,Locus.ID==loc.id)$symbol %>% as.character(.) %>% unique(.))
+}
+
+
+map_tissue <- function(tiss){
+  num <- ifelse(tiss=="islet",1,
+                ifelse(tiss=="muscle",2,
+                       ifelse(tiss=="adipose",3,4)))
+  return(num)
+}
+
+handle_coding <- function(loc.id,ppa){
+  sym <- locid_to_symbol(loc.id)
+  sub <- filter(ess.df,GeneName==sym)
+  if (dim(sub)[1] == 1){
+    score.vec <- c(sub$islet.score,sub$muscle.score,sub$adipose.score,sub$liver.score)
+    if (any(is.na(score.vec)==TRUE)){
+      score.vec <- c(0.25,0.25,0.25,0.25)
+    }
+  } else{
+    score.vec <- c(0.25,0.25,0.25,0.25)
+  }
+  divvy.vec <- ppa * score.vec
+  return(divvy.vec)
+}
+
+handle_annotation <- function(annot.df, annot.name, ppa){
+  sub <- filter(annot.df,grepl(pattern=annot.name,V4))
+  if (dim(sub)[1]>1){
+    print(sub)
+  }
+  if (dim(sub)[1]==0){
+    divvy.vec <- c(0,0,0,0)
+  } else if(grepl(pattern="_specific_",x=sub$V4)){
+    tiss <- strsplit(x=sub$V4,split="_specific_")[[1]][1]
+    num <- map_tissue(tiss)
+    divvy.vec <- c(0,0,0,0)
+    divvy.vec[num] <- ppa
+  } else if(grepl(pattern="all_shared",x=sub$V4)){
+    divvy.vec <- rep(ppa/4,4)
+  } else if(grepl(pattern="shared.",x=sub$V4,fixed=TRUE)){
+    vec <- strsplit(x=sub$V4,split=".",fixed=TRUE)[[1]]
+    tiss.vec <- vec[2:(length(vec)-1)]
+    ppa.val <- ppa/length(tiss.vec)
+    divvy.vec <- c(0,0,0,0)
+    index.vec <- map(tiss.vec,function(t){map_tissue(t)}) %>% as.integer(.)
+    divvy.vec[index.vec] <- ppa.val
+  } else{
+    divvy.vec <- NA
+  }
+  return(divvy.vec)
+}
+
+
+
+divvy_ppa_snp <- function(loc.id,snp.id,mode="full",weights=TRUE){
+  # set weights=NULL if unweighted
+  sub.df <- filter(cred.df,Locus.ID==loc.id,SNPID==snp.id)
+  if (dim(sub.df)[1]>1){
+    ppa <- sub.df$PPA %>% sum(.)
+    sub.df <- sub.df[1,]
+    print("\nduplicate snp: " %&% snp.id %&% " : Locus.ID " %&% loc.id)
+    chrom <- sub.df$CHR; pos <- sub.df$POS;
+    coding <- sub.df$coding  
+  } else{
+    chrom <- sub.df$CHR; pos <- sub.df$POS;
+    ppa <- sub.df$PPA; coding <- sub.df$coding    
+  }
+  if (coding==1){
+    coding.vec <- handle_coding(loc.id,ppa) # islet, muscle, adipose, liver
+  } else{
+    coding.vec <- c(0,0,0,0)
+  }
+  annot.df <- filter(states.df,V1==chrom,V2<=pos,V3>=pos)
+  strongenh.vec <- handle_annotation(annot.df,"strong_enhancer",ppa)
+  weakenh.vec <- handle_annotation(annot.df,"weak_enhancer",ppa)
+  genetrans.vec <- handle_annotation(annot.df,"gene_transcription",ppa)
+  prom.vec <- handle_annotation(annot.df,"promoter",ppa)
+  genenh.vec <- handle_annotation(annot.df,"genic_enhancer",ppa)
+  if (weights==TRUE){
+    c.df <- filter(weight.all.df,annotation=="coding")
+    c.scores <- c(2^(filter(c.df,tissue=="islet")$weight), 2^(filter(c.df,tissue=="muscle")$weight),
+                  2^(filter(c.df,tissue=="adipose")$weight),2^(filter(c.df,tissue=="liver")$weight))
+    coding.vec <- coding.vec  * c.scores
+
+    se.df <- filter(weight.all.df,annotation=="strong.enhancers")
+    se.scores <- c(2^(filter(se.df,tissue=="islet")$weight), 2^(filter(se.df,tissue=="muscle")$weight),
+                   2^(filter(se.df,tissue=="adipose")$weight),2^(filter(se.df,tissue=="liver")$weight))
+    strongenh.vec <- strongenh.vec * se.scores
+
+    we.df <- filter(weight.all.df,annotation=="weak.enhancers")
+    we.scores <- c(2^(filter(we.df,tissue=="islet")$weight), 2^(filter(we.df,tissue=="muscle")$weight),
+                   2^(filter(we.df,tissue=="adipose")$weight),2^(filter(we.df,tissue=="liver")$weight))
+    weakenh.vec <- weakenh.vec * we.scores
+
+    gt.df <- filter(weight.all.df,annotation=="gene.transcription")
+    gt.scores <- c(2^(filter(gt.df,tissue=="islet")$weight), 2^(filter(gt.df,tissue=="muscle")$weight),
+                   2^(filter(gt.df,tissue=="adipose")$weight),2^(filter(gt.df,tissue=="liver")$weight))
+    genetrans.vec <- genetrans.vec * gt.scores
+
+    pr.df <- filter(weight.all.df,annotation=="promoters")
+    pr.scores <- c(2^(filter(pr.df,tissue=="islet")$weight), 2^(filter(pr.df,tissue=="muscle")$weight),
+                   2^(filter(pr.df,tissue=="adipose")$weight), 2^(filter(pr.df,tissue=="liver")$weight))
+    prom.vec <- prom.vec * pr.scores
+
+    ge.df <- filter(weight.all.df,annotation=="genic.enhancer")
+    ge.scores <- c(2^(filter(ge.df,tissue=="islet")$weight),2^(filter(ge.df,tissue=="muscle")$weight),
+                   2^(filter(ge.df,tissue=="adipose")$weight),2^(filter(ge.df,tissue=="liver")$weight))
+    genenh.vec <- genenh.vec * ge.scores
+  }
+  if (mode=="full"){
+    score.vec <- coding.vec + strongenh.vec + weakenh.vec + genenh.vec + prom.vec + genetrans.vec
+  } else if(mode=="coding+strongenhancers"){
+    score.vec <- coding.vec + strongenh.vec
+  } else{
+    print("Need to enter acceptable mode")
+  }
+  if (sum(score.vec)>0){
+    score.vec <- score.vec / (sum(score.vec))
+  } else{
+    score.vec <- c(0,0,0,0)
+  }
+  ppa.vec <- ppa * score.vec
+  names(ppa.vec) <- c("islet","muscle","adipose","liver")
+  out.df <- as.data.frame(t(ppa.vec))
+  return(out.df)
+}
+
+divvy_ppa_loc <- function(loc.id,mode="full",weights=TRUE,scaled=FALSE){
+  sub.df <- filter(cred.df,Locus.ID==loc.id)
+  out.df <- c()
+  snp.vec <- sub.df$SNPID %>% unique(.)
+  pb <- txtProgressBar(min=0,max=length(snp.vec),style=3)
+  for (i in 1:length(snp.vec)){
+    setTxtProgressBar(pb,i)
+    snp.id <- snp.vec[i]
+    build.df <- divvy_ppa_snp(loc.id,snp.id,mode,weights)
+    out.df <- rbind(out.df,build.df)
+  }
+  mat <- as.matrix(out.df)
+  vec <- colSums(mat)
+  cumppa <- sub.df$PPA %>% sum(.)
+  other <- ifelse((cumppa-sum(vec))<=1&(cumppa-sum(vec))>=0,cumppa-sum(vec),0)
+  vec <- c(vec,other)
+  names(vec)[length(vec)] <- "other"
+  if (scaled==TRUE){
+    # Scale ppa's to add up to 1
+    vec <- vec/sum(vec)
+  }
+  out.df <- as.data.frame(t(vec))
+  out.df <- data.frame(Locus.ID=loc.id,out.df)
+  out.df$Locus.ID <- out.df$Locus.ID %>% as.character(.)
+  return(out.df)
+}
+
+build_ppa_partition_df <- function(mode="full",weights=TRUE,scaled=FALSE){
+  loc.ids <- cred.df$Locus.ID %>% unique(.)
+  out.df <- c()
+  pb <- txtProgressBar(min=0,max=length(loc.ids),style=3)
+  for (i in 1:length(loc.ids)){
+    setTxtProgressBar(pb,i)
+    loc.id <- loc.ids[i]
+    print(loc.id)
+    build.df <- divvy_ppa_loc(loc.id,mode,weights,scaled)
+    out.df <- rbind(out.df,build.df)
+  }
+  return(out.df)
+}
+
+
+
+#Generate and save tables
+
+part.fullWU.df <- build_ppa_partition_df(mode="full",weights=TRUE,scaled=FALSE)
+
+
+
+write.table(x=part.fullWU.df,file=out.dir%&%"tissue_ppa_divvy-full-weighted-unscaled.txt",
+            sep="\t",quote=FALSE,row.names=FALSE)
+
+
+
---
+++ revamp/04_V_eQTL-enrichment_specific-downsampled.R
@@ -0,0 +1,201 @@
+set.seed(1)
+
+"%&%" <- function(a,b) paste0(a,b)
+
+library("tidyverse")
+library("data.table")
+library("Homo.sapiens")
+
+
+serv.dir1 <- "/well/got2d/jason/" # 
+#serv.dir1 <- "/home/jason/science/servers/FUSE/"
+serv.dir2 <- "/well/mccarthy/users/jason/" #
+#serv.dir2 <- "/home/jason/science/servers/FUSE5/"
+
+proj.dir <- serv.dir2 %&% "projects/t2d_classification/"
+work.dir <- proj.dir %&% "method_C/"
+
+gwas.dir <- serv.dir1 %&% "reference/gwas/diamante-ukbb_hrc/"
+gwas.df <- fread(gwas.dir %&% "ukbb_diamante-euro.bed") # will take a while to load, 0.681 GB file
+names(gwas.df) <- c("CHR","POS0","POS","SNPID","Freq","Z","PVAL","NCASE","NCONTROL")
+
+pb <- txtProgressBar(min=0,max=dim(gwas.df)[1],style=3)
+gwas.df$MAF <- map(1:dim(gwas.df)[1],function(i){
+  setTxtProgressBar(pb,i)
+  freq <- gwas.df$Freq[i]
+  return(min(freq,1-freq))
+}) %>% as.numeric(.)
+
+#g.df <- dplyr::select(gwas.df,SNPID,MAF); g.df <- g.df[!duplicated(g.df),]
+
+rare.snps <- filter(gwas.df, MAF < 0.005)$SNPID
+low.freq.snps <- filter(gwas.df, MAF >= 0.005 & MAF < 0.05)$SNPID
+high.freq.snps <- filter(gwas.df, MAF >= 0.05)$SNPID
+
+
+gtex.dir <- serv.dir2 %&% "datasets/GTEx/v7/eqtl/GTEx_Analysis_v7_eQTL/"
+
+liv.df <- fread("cat " %&% gtex.dir %&% "Liver.v7.signif_variant_gene_pairs.txt.gz" %&% " | zmore")
+mus.df <- fread("cat " %&% gtex.dir %&% "Muscle_Skeletal.v7.signif_variant_gene_pairs.txt.gz" %&% " | zmore")
+adi.df <- fread("cat " %&% gtex.dir %&% "Adipose_Subcutaneous.v7.signif_variant_gene_pairs.txt.gz" %&% " | zmore")
+
+pb <- txtProgressBar(min=0,max=dim(liv.df)[1],style=3)
+liv.eqtls <- map(1:length(liv.df$variant_id),function(i){
+  setTxtProgressBar(pb,i)
+  vec <- liv.df$variant_id[i] %>% strsplit(.,split="_")
+  id <- "chr" %&% vec[[1]][1] %&% ":" %&% vec[[1]][2]
+}) %>% as.character(.)
+
+pb <- txtProgressBar(min=0,max=dim(mus.df)[1],style=3)
+mus.eqtls <- map(1:length(mus.df$variant_id),function(i){
+  setTxtProgressBar(pb,i)
+  vec <- mus.df$variant_id[i] %>% strsplit(.,split="_")
+  id <- "chr" %&% vec[[1]][1] %&% ":" %&% vec[[1]][2]
+}) %>% as.character(.)
+
+pb <- txtProgressBar(min=0,max=dim(adi.df)[1],style=3)
+adi.eqtls <- map(1:length(adi.df$variant_id),function(i){
+  setTxtProgressBar(pb,i)
+  vec <- adi.df$variant_id[i] %>% strsplit(.,split="_")
+  id <- "chr" %&% vec[[1]][1] %&% ":" %&% vec[[1]][2]
+}) %>% as.character(.)
+
+
+islet.dir <- serv.dir1 %&% "reference/islet/eqtls/oxford/nominal_pass/output/"
+islet.df <- fread("cat " %&% islet.dir %&% "snp_keyfile_fdr05.txt.gz" %&% " | zmore")
+pb <- txtProgressBar(min=0,max=dim(islet.df)[1],style=3)
+islet.eqtls <- map(1:dim(islet.df)[1],function(i){
+  setTxtProgressBar(pb,i)
+  vec <- c(islet.df$CHR[i],islet.df$POS[i])
+  id <- vec[1] %&% ":" %&% vec[2]
+}) %>% as.character(.)
+
+
+isl.spec <- islet.eqtls[!(islet.eqtls %in% c(mus.eqtls,liv.eqtls,adi.eqtls))]
+mus.spec <- mus.eqtls[!(mus.eqtls %in% c(islet.eqtls,liv.eqtls,adi.eqtls))]
+adi.spec <- adi.eqtls[!(adi.eqtls %in% c(islet.eqtls,liv.eqtls,mus.eqtls))]
+liv.spec <- liv.eqtls[!(liv.eqtls %in% c(islet.eqtls,adi.eqtls,mus.eqtls))]
+
+# min number, liver, n=37391
+
+min.val <- min(c(length(isl.spec),length(mus.spec),
+                 length(adi.spec),length(liv.spec)))
+
+islet.eqtls <- sample(x=isl.spec,replace=FALSE,size=min.val)
+mus.eqtls <- sample(x=mus.spec,replace=FALSE,size=min.val)
+liv.eqtls <- sample(x=liv.spec,replace=FALSE,size=min.val)
+adi.eqtls <- sample(x=adi.spec,replace=FALSE,size=min.val)
+
+
+get_overlap <- function(query.vec,eqtl.vec){
+  sum(query.vec %in% eqtl.vec)
+}
+
+get_maf_matched_null_vec <- function(query.vec){
+  sub.df <- filter(gwas.df,SNPID%in%query.vec)
+  non.dup.df <- sub.df[!duplicated(sub.df$SNPID),]  # dup.df <- sub.df[duplicated(sub.df$SNPID),]
+  maf.vec <- non.dup.df$MAF
+  rare.count <- sum(maf.vec < 0.005)
+  low.freq.count <- sum(maf.vec >= 0.005 & maf.vec < 0.05) # MAF >= 0.005 & MAF < 0.05
+  high.freq.count <- sum(maf.vec >= 0.05) # MAF >= 0.05
+  rare.samp <- sample(rare.snps,size=rare.count,replace=FALSE)
+  low.freq.samp <- sample(low.freq.snps,size=low.freq.count,replace=FALSE)
+  high.freq.samp <- sample(high.freq.snps,size=high.freq.count,replace=FALSE)
+  null.vec <- c(rare.samp,low.freq.samp,high.freq.samp)
+  return(null.vec)
+}
+
+enrich_test <- function(query.vec,eqtl.vec,iter){
+  obs <- get_overlap(query.vec,eqtl.vec)
+  null.counts <- c()
+  pb <- txtProgressBar(min=0,max=iter,style=3)
+  for (i in 1:iter){
+    setTxtProgressBar(pb,i)
+    null.vec <- get_maf_matched_null_vec(query.vec)
+    nullcount <- get_overlap(null.vec,eqtl.vec)
+    null.counts <- append(null.counts,nullcount)
+  }
+  pval <- (sum(null.counts >= obs) + 1)/ (iter + 1)
+  fac <- obs / mean(null.counts)
+  out.df <- data.frame(observed=obs,enrichment=fac,pvalue=pval,stringsAsFactors = FALSE)
+}
+
+enrich_test_across_tissue <- function(query.vec,iter){
+  print("islets..."); df1 <- enrich_test(query.vec,islet.eqtls,iter)
+  print("liver..."); df2 <- enrich_test(query.vec,liv.eqtls,iter)
+  print("adipose..."); df3 <- enrich_test(query.vec,adi.eqtls,iter)
+  print("muscle..."); df4 <- enrich_test(query.vec,mus.eqtls,iter)
+  out.df <- rbind(tissue=c("islet","liver","adipose","mucle"),df1,df2,df3,df4)
+  out.df$tissue <- as.character(out.df$tissue)
+  return(out.df)
+}
+
+evaluate_threshold <- function(input.df,classified,iter,threshold){
+  islet.loci <- input.df$Locus.ID[classified=="islet"]
+  liver.loci <- input.df$Locus.ID[classified=="liver"]
+  adipose.loci <- input.df$Locus.ID[classified=="adipose"]
+  muscle.loci <- input.df$Locus.ID[classified=="muscle"]
+  islet.snps <- filter(cred.df,Locus.ID %in% islet.loci)$SNPID
+  liver.snps <- filter(cred.df,Locus.ID %in% liver.loci)$SNPID
+  adipose.snps <- filter(cred.df,Locus.ID %in% adipose.loci)$SNPID
+  muscle.snps <- filter(cred.df,Locus.ID %in% muscle.loci)$SNPID
+  
+  df1a <- enrich_test(islet.snps,islet.eqtls,iter)
+  df1b <- enrich_test(islet.snps,liv.eqtls,iter)
+  df1c <- enrich_test(islet.snps,adi.eqtls,iter)
+  df1d <- enrich_test(islet.snps,mus.eqtls,iter)
+  
+  df2a <- enrich_test(liver.snps,islet.eqtls,iter)
+  df2b <- enrich_test(liver.snps,liv.eqtls,iter)
+  df2c <- enrich_test(liver.snps,adi.eqtls,iter)
+  df2d <- enrich_test(liver.snps,mus.eqtls,iter)
+  
+  df3a <- enrich_test(adipose.snps,islet.eqtls,iter)
+  df3b <- enrich_test(adipose.snps,liv.eqtls,iter)
+  df3c <- enrich_test(adipose.snps,adi.eqtls,iter)
+  df3d <- enrich_test(adipose.snps,mus.eqtls,iter)
+  
+  df4a <- enrich_test(muscle.snps,islet.eqtls,iter)
+  df4b <- enrich_test(muscle.snps,liv.eqtls,iter)
+  df4c <- enrich_test(muscle.snps,adi.eqtls,iter)
+  df4d <- enrich_test(muscle.snps,mus.eqtls,iter)
+  
+  
+  df0 <- data.frame(tissue=c(rep("islet",4),rep("liver",4),rep("adipose",4),rep("muscle",4)),
+                    eQTL=rep(c("islet","liver","adipose","muscle"),4))
+  out.df <- rbind(df1a,df1b,df1c,df1d,df2a,df2b,df2c,df2d,df3a,df3b,df3c,df3d,df4a,df4b,df4c,df4d)
+  out.df <- cbind(df0,out.df)
+  out.df$tissue <- as.character(out.df$tissue)
+  out.df$threshold <- rep(threshold,dim(out.df)[1])
+  return(out.df)
+}
+
+evaluate_thresholds <- function(input.df,iter){
+  print("Threshold:");print("0.00")
+  df00 <- evaluate_threshold(input.df,classified=input.df$assigned_00,iter,"0.00")
+  print("Threshold:");print("0.20")
+  df20 <- evaluate_threshold(input.df,classified=input.df$assigned_20,iter,"0.20")
+  print("Threshold:");print("0.50")
+  df50 <- evaluate_threshold(input.df,classified=input.df$assigned_50,iter,"0.50")
+  print("Threshold:");print("0.80")
+  df80 <- evaluate_threshold(input.df,classified=input.df$assigned_80,iter,"0.80")
+  out.df <- rbind(df00,df20,df50,df80)
+  return(out.df)
+}
+
+
+
+cred.dir <- serv.dir2 %&% "projects/t2d_classification/method_C/genetic_credible_sets/"
+cred.df <- fread(cred.dir %&% "gencred.txt") %>% filter(.,PPA>=0.01)
+# PPA 1% filter reduces number of query SNPs from 126K to 5,010 SNPs 
+
+#input1.df <- fread(proj.dir %&% "method_C/analysis_files/classified-loci_unweighted.txt")
+#thresh1.df <- evaluate_thresholds(input1.df,iter=1000)
+#write.table(x=thresh1.df,file=proj.dir%&%"method_C/analysis_files/downsampled_eqtl-specific-validation_unweighted.txt",
+#            sep="\t",quote=FALSE,row.names=FALSE)
+
+input2.df <- fread(proj.dir %&% "method_C/analysis_files/classified-loci_weighted.txt")
+thresh2.df <- evaluate_thresholds(input2.df,iter=1000)
+write.table(x=thresh2.df,file=proj.dir%&%"method_C/analysis_files/downsampled_eqtl-specific-validation_weighted.txt",
+            sep="\t",quote=FALSE,row.names=FALSE)
+
