a4a4536569621e4c27c99103a0d8d5f0f3ad2246
Jason Torres <jason.matthew.torres@gmail.com>
2020-04-29 09:06:43 +0100

    ran eqtl enrichment tests

--- tactical_analyses/04.2_run-enrichment-tests.R
+++ tactical_analyses/04.2_run-enrichment-tests.R
@@ -12,11 +12,11 @@ library("dplyr")
 library("purrr")
 library("data.table")
 
-serv.dir1 <- "/well/got2d/jason/" ## "/home/jason/science/servers/FUSE/" # 
-serv.dir2 <- "/well/mccarthy/users/jason/" #"/home/jason/science/servers/FUSE5/" #
+serv.dir1 <- "/home/jason/science/servers/FUSE/" # "/well/got2d/jason/" ## 
+serv.dir2 <- "/home/jason/science/servers/FUSE5/" #"/well/mccarthy/users/jason/" #
 local.dir <- "/home/jason/science/projects/t2d_classification/revamp/"
 proj.dir <- serv.dir2 %&% "projects/t2d_classification/"
-work.dir <- proj.dir %&% "revamp/"
+work.dir <- proj.dir %&% "tactical_analyses/"
 out.dir <- work.dir %&% "enrichment_files/eqtls/"
 
 toa.df <- fread(work.dir %&% "analysis_files/classified-loci_weighted_with-shared.txt")
@@ -175,4 +175,3 @@ build_enrich_df <- function(){
 # RUN
 enrich.df <- build_enrich_df()
 write.table(x=enrich.df,file=out.dir %&% "eqtl_enrichment.txt",sep="\t",quote=F,row.names=F)
-#write.table(x=enrich.df,file=local.dir %&% "eqtl_enrichment.txt",sep="\t",quote=F,row.names=F)
