365bd37e136cc800e98c184efedae56df2b6c60f
Jason Torres <jasont@NDPH7917.local>
2020-08-20 16:04:22 +0100

    updated and ran code for R1.2.2 script

--- tactical_analyses/revisions/reviewer_1/R1.2.2_chromstate-enrich.R
+++ tactical_analyses/revisions/reviewer_1/R1.2.2_chromstate-enrich.R
@@ -4,20 +4,23 @@ library("tidyverse")
 library("data.table")
 library("GenomicRanges")
 
-fuse.path <- "/home/jason/science/servers/"
+fuse.path <- "/Users/jasont/science/servers/"
 serv.dir <- fuse.path %&% "FUSE5/"
 serv.dir0 <- fuse.path %&% "FUSE/"
 
 proj.dir <- serv.dir %&% "projects/t2d_classification/"
-work.dir <- proj.dir %&% "tactical_analyses/"
+work.dir <- proj.dir %&% "tactical_analyses/revisions/reviewer_1/"
 analysis.dir <- work.dir %&% "analysis_files/"
 
-cred.df <- fread(work.dir %&% "genetic_credible_sets/gencred.txt")
-class.df <- fread(analysis.dir %&% "classified-loci_weighted_with-shared.txt")
+cred.df <- fread(proj.dir %&%"tactical_analyses/genetic_credible_sets/gencred.txt")
+class.df <- fread(proj.dir %&% "tactical_analyses/analysis_files/"
+                  %&% "classified-loci_weighted_with-shared.txt")
+het.df <- fread(work.dir %&% "bmi-het-signals.txt")
+het.sigs <- het.df$Locus.ID 
+non.het.sigs <- class.df$Locus.ID[!(class.df$Locus.ID %in% het.sigs)]
 
-states.df <- fread(serv.dir0 %&% "reference/chromatin_segmentation/varshney_2016/bed_files/complete_varshney_chromHMM_states.bed")
-
-
+states.df <- fread(serv.dir0 %&%
+ "reference/chromatin_segmentation/varshney_2016/bed_files/complete_varshney_chromHMM_states.bed")
 
 df_to_gr <- function(df){
   gr <- GRanges(seqnames=df$V1,IRanges(start=df$V2,end=df$V3))
@@ -71,9 +74,9 @@ brain.tss <- filter(states.df,grepl(pattern = "Active_TSS",x=V4)) %>%
 
 
 
-get_cum_score <- function(assigned="unclassified",query.df){
+get_cum_score <- function(sigs,query.df){
   query.gr <- GRanges(seqnames = query.df$V1, IRanges(query.df$V2,query.df$V3))
-  sigs <- filter(class.df,assigned_20==assigned)$Locus.ID
+  #sigs <- filter(class.df,assigned_20==assigned)$Locus.ID
   cumsum.total <- filter(cred.df,CondID%in%sigs)$PPA %>% sum(.)
   cumsum.state <- 0 
   out.df <- c()
@@ -99,161 +102,103 @@ get_cum_score <- function(assigned="unclassified",query.df){
 
 
 
-unc.t2d.qui <- get_cum_score("unclassified",t2d.qui)
-unc.t2d.rep <- get_cum_score("unclassified",t2d.rep)
-unc.brain.enh <- get_cum_score("unclassified",brain.enh)
-unc.brain.tss <- get_cum_score("unclassified",brain.tss)
+het.t2d.qui <- get_cum_score(het.sigs,t2d.qui)
+saveRDS(het.t2d.qui,file=work.dir %&% "het.t2d.qui.RDS")
+non.t2d.qui <- get_cum_score(non.het.sigs,t2d.qui)
+saveRDS(non.t2d.qui,file=work.dir %&% "non.t2d.qui.RDS")
 
-isl.t2d.qui <- get_cum_score("islet",t2d.qui)
-isl.t2d.rep <- get_cum_score("islet",t2d.rep)
-isl.brain.enh <- get_cum_score("islet",brain.enh)
-isl.brain.tss <- get_cum_score("islet",brain.tss)
+het.t2d.rep <- get_cum_score(het.sigs,t2d.rep)
+saveRDS(het.t2d.rep,file=work.dir %&% "het.t2d.rep.RDS")
+non.t2d.rep <- get_cum_score(non.het.sigs,t2d.rep)
+saveRDS(non.t2d.rep,file=work.dir %&% "non.t2d.rep.RDS")
 
-adi.t2d.qui <- get_cum_score("adipose",t2d.qui)
-adi.t2d.rep <- get_cum_score("adipose",t2d.rep)
-adi.brain.enh <- get_cum_score("adipose",brain.enh)
-adi.brain.tss <- get_cum_score("adipose",brain.tss)
+het.brain.enh <- get_cum_score(het.sigs,brain.enh)
+saveRDS(het.brain.enh,file=work.dir %&% "het.brain.enh.RDS")
+non.brain.enh <- get_cum_score(non.het.sigs,brain.enh)
+saveRDS(non.brain.enh,file=work.dir %&% "non.brain.enh.RDS")
 
-mus.t2d.qui <- get_cum_score("muscle",t2d.qui)
-mus.t2d.rep <- get_cum_score("muscle",t2d.rep)
-mus.brain.enh <- get_cum_score("muscle",brain.enh)
-mus.brain.tss <- get_cum_score("muscle",brain.tss)
-
-liv.t2d.qui <- get_cum_score("liver",t2d.qui)
-liv.t2d.rep <- get_cum_score("liver",t2d.rep)
-liv.brain.enh <- get_cum_score("liver",brain.enh)
-liv.brain.tss <- get_cum_score("liver",brain.tss)
-
-sha.t2d.qui <- get_cum_score("shared",t2d.qui)
-sha.t2d.rep <- get_cum_score("shared",t2d.rep)
-sha.brain.enh <- get_cum_score("shared",brain.enh)
-sha.brain.tss <- get_cum_score("shared",brain.tss)
-
+het.brain.tss <- get_cum_score(het.sigs,brain.tss)
+saveRDS(het.brain.tss,file=work.dir %&% "het.brain.tss.RDS")
+non.brain.tss <- get_cum_score(non.het.sigs,brain.tss)
+saveRDS(non.brain.tss,file=work.dir %&% "non.brain.tss.RDS")
 
 
 # Repressed regions in the four T2D tissues
 
-
-
-unc.t2d.rep[[3]]$assigned_20 <- "unclassified"
-isl.t2d.rep[[3]]$assigned_20 <- "islet"
-mus.t2d.rep[[3]]$assigned_20 <- "muscle"
-liv.t2d.rep[[3]]$assigned_20 <- "liver"
-adi.t2d.rep[[3]]$assigned_20 <- "adipose"
-sha.t2d.rep[[3]]$assigned_20 <- "shared"
-
-rep.df <- rbind(unc.t2d.rep[[3]],isl.t2d.rep[[3]],mus.t2d.rep[[3]],
-                liv.t2d.rep[[3]],adi.t2d.rep[[3]],sha.t2d.rep[[3]])
-rep.df$classified <- map(rep.df$assigned_20,function(s){
-  ifelse(s=="unclassified","unclassified","classified")
+rep.df <- rbind(het.t2d.rep[[3]],non.t2d.rep[[3]])
+rep.df$classified <- map(rep.df$Locus.ID,function(s){
+  ifelse(s%in%het.sigs,"hetero","non.hetero")
 }) %>% as.character(.)
 
 rep.plot <- ggplot(data=rep.df,aes(x=classified,y=CumPPA)) + 
   geom_boxplot() + 
   geom_jitter(shape=15,color="steelblue",position=position_jitter(0.21))
 
-filter(rep.df,classified=="classified")$CumPPA %>% mean(.)
-filter(rep.df,classified=="unclassified")$CumPPA %>% mean(.)
-wilcox.test(filter(rep.df,classified=="classified")$CumPPA,
-            filter(rep.df,classified=="unclassified")$CumPPA)
-
-
+filter(rep.df,classified=="hetero")$CumPPA %>% mean(.)
+filter(rep.df,classified=="non.hetero")$CumPPA %>% mean(.)
+wilcox.test(filter(rep.df,classified=="hetero")$CumPPA,
+            filter(rep.df,classified=="non.hetero")$CumPPA)
 
 # Quiescent regions in the four T2D tissues 
 
 
 
-unc.t2d.qui[[3]]$assigned_20 <- "unclassified"
-isl.t2d.qui[[3]]$assigned_20 <- "islet"
-mus.t2d.qui[[3]]$assigned_20 <- "muscle"
-liv.t2d.qui[[3]]$assigned_20 <- "liver"
-adi.t2d.qui[[3]]$assigned_20 <- "adipose"
-sha.t2d.qui[[3]]$assigned_20 <- "shared"
-
-qui.df <- rbind(unc.t2d.qui[[3]],isl.t2d.qui[[3]],mus.t2d.qui[[3]],
-                liv.t2d.qui[[3]],adi.t2d.qui[[3]],sha.t2d.qui[[3]])
-qui.df$classified <- map(qui.df$assigned_20,function(s){
-  ifelse(s=="unclassified","unclassified","classified")
+qui.df <- rbind(het.t2d.qui[[3]],non.t2d.qui[[3]])
+qui.df$classified <- map(qui.df$Locus.ID,function(s){
+  ifelse(s%in%het.sigs,"hetero","non.hetero")
 }) %>% as.character(.)
 
 qui.plot <- ggplot(data=qui.df,aes(x=classified,y=CumPPA)) + 
   geom_boxplot() + 
   geom_jitter(shape=15,color="steelblue",position=position_jitter(0.21))
-filter(qui.df,classified=="classified")$CumPPA %>% mean(.)
-filter(qui.df,classified=="unclassified")$CumPPA %>% mean(.)
-wilcox.test(filter(qui.df,classified=="classified")$CumPPA,
-            filter(qui.df,classified=="unclassified")$CumPPA)
 
+filter(qui.df,classified=="hetero")$CumPPA %>% mean(.)
+filter(qui.df,classified=="non.hetero")$CumPPA %>% mean(.)
+wilcox.test(filter(qui.df,classified=="hetero")$CumPPA,
+            filter(qui.df,classified=="non.hetero")$CumPPA)
 
-filter(qui.df,classified=="unclassified")$CumPPA %>% mean(.)
-filter(rep.df,classified=="unclassified")$CumPPA %>% mean(.)
-wilcox.test(filter(qui.df,classified=="unclassified")$CumPPA,
-            filter(rep.df,classified=="unclassified")$CumPPA)
 
 
 
 # Brain enhancers 
 
-
-unc.brain.enh[[3]]$assigned_20 <- "unclassified"
-isl.brain.enh[[3]]$assigned_20 <- "islet"
-mus.brain.enh[[3]]$assigned_20 <- "muscle"
-liv.brain.enh[[3]]$assigned_20 <- "liver"
-adi.brain.enh[[3]]$assigned_20 <- "adipose"
-sha.brain.enh[[3]]$assigned_20 <- "shared"
-
-brain.enh.df <- rbind(unc.brain.enh[[3]],isl.brain.enh[[3]],mus.brain.enh[[3]],
-                      liv.brain.enh[[3]],adi.brain.enh[[3]],sha.brain.enh[[3]])
-brain.enh.df$classified <- map(brain.enh.df$assigned_20,function(s){
-  ifelse(s=="unclassified","unclassified","classified")
+brain.enh.df <- rbind(het.brain.enh[[3]],non.brain.enh[[3]])
+brain.enh.df$classified <- map(brain.enh.df$Locus.ID,function(s){
+  ifelse(s%in%het.sigs,"hetero","non.hetero")
 }) %>% as.character(.)
 
-brain.enh.plot <- ggplot(data=brain.enh.df,aes(x=assigned_20,y=CumPPA)) + 
+brain.enh.plot <- ggplot(data=brain.enh.df,aes(x=classified,y=CumPPA)) + 
   geom_boxplot() + 
   geom_jitter(shape=15,color="steelblue",position=position_jitter(0.21))
-brain.enh.plot.v2 <- ggplot(data=brain.enh.df,aes(x=classified,y=CumPPA)) + 
-  geom_boxplot() + 
-  geom_jitter(shape=15,color="steelblue",position=position_jitter(0.21))
-filter(brain.enh.df,classified=="classified")$CumPPA %>% mean(.)
-filter(brain.enh.df,classified=="unclassified")$CumPPA %>% mean(.)
-wilcox.test(filter(brain.enh.df,classified=="classified")$CumPPA,
-            filter(brain.enh.df,classified=="unclassified")$CumPPA)
+
+filter(brain.enh.df,classified=="hetero")$CumPPA %>% mean(.)
+filter(brain.enh.df,classified=="non.hetero")$CumPPA %>% mean(.)
+wilcox.test(filter(brain.enh.df,classified=="hetero")$CumPPA,
+            filter(brain.enh.df,classified=="non.hetero")$CumPPA)
+
 
 
 # Brain promoters 
 
-
-unc.brain.tss[[3]]$assigned_20 <- "unclassified"
-isl.brain.tss[[3]]$assigned_20 <- "islet"
-mus.brain.tss[[3]]$assigned_20 <- "muscle"
-liv.brain.tss[[3]]$assigned_20 <- "liver"
-adi.brain.tss[[3]]$assigned_20 <- "adipose"
-sha.brain.tss[[3]]$assigned_20 <- "shared"
-
-brain.tss.df <- rbind(unc.brain.tss[[3]],isl.brain.tss[[3]],mus.brain.tss[[3]],
-                      liv.brain.tss[[3]],adi.brain.tss[[3]],sha.brain.tss[[3]])
-brain.tss.df$classified <- map(brain.tss.df$assigned_20,function(s){
-  ifelse(s=="unclassified","unclassified","classified")
+brain.tss.df <- rbind(het.brain.tss[[3]],non.brain.tss[[3]])
+brain.tss.df$classified <- map(brain.tss.df$Locus.ID,function(s){
+  ifelse(s%in%het.sigs,"hetero","non.hetero")
 }) %>% as.character(.)
 
-brain.tss.plot <- ggplot(data=brain.tss.df,aes(x=assigned_20,y=CumPPA)) + 
-  geom_boxplot() + 
-  geom_jitter(shape=15,color="steelblue",position=position_jitter(0.21))
-brain.tss.plot.v2 <- ggplot(data=brain.tss.df,aes(x=classified,y=CumPPA)) + 
+brain.tss.plot <- ggplot(data=brain.tss.df,aes(x=classified,y=CumPPA)) + 
   geom_boxplot() + 
   geom_jitter(shape=15,color="steelblue",position=position_jitter(0.21))
 
-filter(brain.tss.df,classified=="classified")$CumPPA %>% mean(.)
-filter(brain.tss.df,classified=="unclassified")$CumPPA %>% mean(.)
-wilcox.test(filter(brain.tss.df,classified=="classified")$CumPPA,
-            filter(brain.tss.df,classified=="unclassified")$CumPPA)
+filter(brain.tss.df,classified=="hetero")$CumPPA %>% mean(.)
+filter(brain.tss.df,classified=="non.hetero")$CumPPA %>% mean(.)
+wilcox.test(filter(brain.tss.df,classified=="hetero")$CumPPA,
+            filter(brain.tss.df,classified=="non.hetero")$CumPPA)
 
 
 # Evaluate Fine-mapping resolution 
 
-
-class.df$classified <- map(class.df$assigned_20,function(s){
-  ifelse(s=="unclassified","unclassified","classified")
+class.df$classified <- map(class.df$Locus.ID,function(s){
+  ifelse(s%in%het.sigs,"hetero","non.hetero")
 }) %>% as.character(.)
 class.df$maxppa <- map(class.df$Locus.ID,function(id){
   (filter(cred.df,CondID==id) %>% arrange(.,desc(PPA)))$PPA[1]
@@ -263,50 +208,49 @@ class.df$ncredsnps <- map(class.df$Locus.ID,function(id){
 }) %>% as.integer(.)
 
 
-filter(class.df,classified=="classified")$maxppa %>% mean(.)
-filter(class.df,classified=="classified")$maxppa %>% median(.)
-filter(class.df,classified=="unclassified")$maxppa %>% mean(.)
-filter(class.df,classified=="unclassified")$maxppa %>% median(.)
-wilcox.test(filter(class.df,classified=="classified")$maxppa,
-            filter(class.df,classified=="unclassified")$maxppa)
+filter(class.df,classified=="hetero")$maxppa %>% mean(.)
+filter(class.df,classified=="hetero")$maxppa %>% median(.)
+filter(class.df,classified=="non.hetero")$maxppa %>% mean(.)
+filter(class.df,classified=="non.hetero")$maxppa %>% median(.)
+wilcox.test(filter(class.df,classified=="hetero")$maxppa,
+            filter(class.df,classified=="non.hetero")$maxppa)
 
-filter(class.df,classified=="classified")$ncredsnps %>% mean(.)
-filter(class.df,classified=="classified")$ncredsnps %>% median(.)
-filter(class.df,classified=="unclassified")$ncredsnps %>% mean(.)
-filter(class.df,classified=="unclassified")$ncredsnps %>% median(.)
-wilcox.test(filter(class.df,classified=="classified")$ncredsnps,
-            filter(class.df,classified=="unclassified")$ncredsnps)
+filter(class.df,classified=="hetero")$ncredsnps %>% mean(.)
+filter(class.df,classified=="hetero")$ncredsnps %>% median(.)
+filter(class.df,classified=="non.hetero")$ncredsnps %>% mean(.)
+filter(class.df,classified=="non.hetero")$ncredsnps %>% median(.)
+wilcox.test(filter(class.df,classified=="hetero")$ncredsnps,
+            filter(class.df,classified=="non.hetero")$ncredsnps)
 
 
+rep.class <- filter(rep.df,classified=="hetero")$CumPPA %>% mean(.)
+rep.unclass <- filter(rep.df,classified=="non.hetero")$CumPPA %>% mean(.)
+rep.pval <- wilcox.test(filter(rep.df,classified=="hetero")$CumPPA,
+                        filter(rep.df,classified=="non.hetero")$CumPPA)$p.value
 
-rep.class <- filter(rep.df,classified=="classified")$CumPPA %>% mean(.)
-rep.unclass <- filter(rep.df,classified=="unclassified")$CumPPA %>% mean(.)
-rep.pval <- wilcox.test(filter(rep.df,classified=="classified")$CumPPA,
-                        filter(rep.df,classified=="unclassified")$CumPPA)$p.value
+qui.class <- filter(qui.df,classified=="hetero")$CumPPA %>% mean(.)
+qui.unclass <- filter(qui.df,classified=="non.hetero")$CumPPA %>% mean(.)
+qui.pval <- wilcox.test(filter(qui.df,classified=="hetero")$CumPPA,
+                        filter(qui.df,classified=="non.hetero")$CumPPA)$p.value
 
-qui.class <- filter(qui.df,classified=="classified")$CumPPA %>% mean(.)
-qui.unclass <- filter(qui.df,classified=="unclassified")$CumPPA %>% mean(.)
-qui.pval <- wilcox.test(filter(qui.df,classified=="classified")$CumPPA,
-                        filter(qui.df,classified=="unclassified")$CumPPA)$p.value
+brain.enh.class <- filter(brain.enh.df,classified=="hetero")$CumPPA %>% mean(.)
+brain.enh.unclass <- filter(brain.enh.df,classified=="non.hetero")$CumPPA %>% mean(.)
+brain.enh.pval <- wilcox.test(filter(brain.enh.df,classified=="hetero")$CumPPA,
+                              filter(brain.enh.df,classified=="non.hetero")$CumPPA)$p.value
 
-brain.enh.class <- filter(brain.enh.df,classified=="classified")$CumPPA %>% mean(.)
-brain.enh.unclass <- filter(brain.enh.df,classified=="unclassified")$CumPPA %>% mean(.)
-brain.enh.pval <- wilcox.test(filter(brain.enh.df,classified=="classified")$CumPPA,
-                              filter(brain.enh.df,classified=="unclassified")$CumPPA)$p.value
-
-brain.tss.class <- filter(brain.tss.df,classified=="classified")$CumPPA %>% mean(.)
-brain.tss.unclass <- filter(brain.tss.df,classified=="unclassified")$CumPPA %>% mean(.)
-brain.tss.pval <- wilcox.test(filter(brain.tss.df,classified=="classified")$CumPPA,
-                              filter(brain.tss.df,classified=="unclassified")$CumPPA)$p.value
+brain.tss.class <- filter(brain.tss.df,classified=="hetero")$CumPPA %>% mean(.)
+brain.tss.unclass <- filter(brain.tss.df,classified=="non.hetero")$CumPPA %>% mean(.)
+brain.tss.pval <- wilcox.test(filter(brain.tss.df,classified=="hetero")$CumPPA,
+                              filter(brain.tss.df,classified=="non.hetero")$CumPPA)$p.value
 
 
 out.df <- data.frame(annotation_class=c("Repressed.T2Dtiss","Quiescent.T2Dtiss",
                                         "Enhancer.Brain","ActiveTSS.Brain"),
-                     mean.cum.ppa.classified=c(rep.class,qui.class,brain.enh.class,brain.tss.class),
-                     mean.cum.ppa.unclassified=c(rep.unclass,qui.unclass,brain.enh.unclass,brain.tss.unclass),
+                     mean.cum.ppa.hetero=c(rep.class,qui.class,brain.enh.class,brain.tss.class),
+                     mean.cum.ppa.non.hetero=c(rep.unclass,qui.unclass,brain.enh.unclass,brain.tss.unclass),
                      pval=c(rep.pval,qui.pval,brain.enh.pval,brain.tss.pval),stringsAsFactors = F)
 
-write.table(x=out.df,file=analysis.dir%&%"brain-enrich.txt",sep="\t",quote=F,row.names=F)
+write.table(x=out.df,file=work.dir%&%"heteroBMI-signal-enrich.txt",sep="\t",quote=F,row.names=F)
 
 
 
